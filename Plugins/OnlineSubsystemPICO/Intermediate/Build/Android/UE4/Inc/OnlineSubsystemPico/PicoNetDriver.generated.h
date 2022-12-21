90ebff9a7d39e682a4a95aa28b8eb1bcc3ced728
bayzhen <1521085156@qq.com>
2022-12-21 16:02:22 +0800

    Initial commit

---
+++ Plugins/OnlineSubsystemPICO/Intermediate/Build/Android/UE4/Inc/OnlineSubsystemPico/PicoNetDriver.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ONLINESUBSYSTEMPICO_PicoNetDriver_generated_h
+#error "PicoNetDriver.generated.h already included, missing '#pragma once' in PicoNetDriver.h"
+#endif
+#define ONLINESUBSYSTEMPICO_PicoNetDriver_generated_h
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_SPARSE_DATA
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_RPC_WRAPPERS
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPicoNetDriver(); \
+	friend struct Z_Construct_UClass_UPicoNetDriver_Statics; \
+public: \
+	DECLARE_CLASS(UPicoNetDriver, UIpNetDriver, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OnlineSubsystemPico"), NO_API) \
+	DECLARE_SERIALIZER(UPicoNetDriver)
+
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_INCLASS \
+private: \
+	static void StaticRegisterNativesUPicoNetDriver(); \
+	friend struct Z_Construct_UClass_UPicoNetDriver_Statics; \
+public: \
+	DECLARE_CLASS(UPicoNetDriver, UIpNetDriver, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OnlineSubsystemPico"), NO_API) \
+	DECLARE_SERIALIZER(UPicoNetDriver)
+
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPicoNetDriver(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPicoNetDriver) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPicoNetDriver); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPicoNetDriver); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPicoNetDriver(UPicoNetDriver&&); \
+	NO_API UPicoNetDriver(const UPicoNetDriver&); \
+public:
+
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPicoNetDriver(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPicoNetDriver(UPicoNetDriver&&); \
+	NO_API UPicoNetDriver(const UPicoNetDriver&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPicoNetDriver); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPicoNetDriver); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPicoNetDriver)
+
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_PRIVATE_PROPERTY_OFFSET
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_18_PROLOG
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_PRIVATE_PROPERTY_OFFSET \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_SPARSE_DATA \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_RPC_WRAPPERS \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_INCLASS \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_PRIVATE_PROPERTY_OFFSET \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_SPARSE_DATA \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_INCLASS_NO_PURE_DECLS \
+	Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ONLINESUBSYSTEMPICO_API UClass* StaticClass<class UPicoNetDriver>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Haptic_Plugins_OnlineSubsystemPICO_Source_OnlineSubsystemPico_Classes_PicoNetDriver_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
